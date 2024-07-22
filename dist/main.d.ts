0e9a926def26991115df64a8ee1a6892b28d9896
Blu <plush.gamer.456@gmail.com>
2024-07-21 22:40:33 -0400

    Updated some times

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -1,58 +1,65 @@
 import "typescript";
 export type OmniType = "track" | "list" | "author";
-export type OmniID = String;
-export type OmniURL = String;
-export type ServiceCode = String;
+export type OmniID = string;
+export type OmniURL = string;
+export type ServiceCode = string;
 export interface OmniService {
-    check(input: String): OmniType | null;
+    name: string;
+    code: ServiceCode;
+    check(input: string): OmniType | null;
     get(id: OmniID, type: OmniType): OmniTrack | OmniList | OmniAuthor | null;
     track(input: OmniID | OmniURL): OmniTrack | null;
     list(input: OmniID | OmniURL): OmniList | null;
     author(input: OmniID | OmniURL): OmniAuthor | null;
-    search(query: String, type: OmniType, limit: Number): OmniTrack[] | OmniList[] | OmniAuthor[];
+    search(query: string, type: OmniType, limit: Number): OmniTrack[] | OmniList[] | OmniAuthor[];
 }
 export interface OmniTrack {
     type: "track";
-    title: String;
+    title: string;
     author: StaticOmniAuthor;
-    desc: String;
-    image: OmniURL;
+    desc: string | null;
+    image: OmniURL | null;
     url: OmniURL;
     omni_id: OmniID;
     service: ServiceInfo;
+    likes: number | null;
 }
 export interface OmniList {
     type: "list";
-    title: String;
-    desc: String;
+    title: string;
+    desc: string | null;
+    image: OmniURL | null;
     url: OmniURL;
     omni_id: OmniID;
     tracks: OmniID[] | StaticOmniTrack[];
     service: ServiceInfo;
+    likes: number | null;
 }
 export interface OmniAuthor {
     type: "author";
-    name: String;
-    bio: String;
+    name: string;
+    bio: string;
+    image: OmniURL | null;
     url: OmniURL;
     omni_id: OmniID;
     tracks: OmniID[] | StaticOmniTrack[];
     service: ServiceInfo;
+    followers: number | null;
 }
 export interface StaticOmniTrack {
-    title: String;
+    title: string;
     author: StaticOmniAuthor;
     omni_id: OmniID;
     service: ServiceInfo;
 }
 export interface StaticOmniAuthor {
-    name: String;
+    name: string;
     url: OmniURL;
     omni_id: OmniID;
 }
 export interface ServiceInfo {
-    id: String;
+    id: string;
     code: ServiceCode;
-    name: String;
+    name: string;
 }
 //# sourceMappingURL=main.d.ts.map
